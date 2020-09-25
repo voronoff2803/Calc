7f997e0b9e1fa45c008618a36799e8e6a8cdaa6e
Алексей <voronoff2803@gmail.com>
2020-09-26 01:44:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.example.calc
+
+import android.graphics.Color
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.util.Size
+import android.util.TypedValue.COMPLEX_UNIT_SP
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var slectedButton: Button? = null
+    var lastResult = ""
+    var isActionJustSelected = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textView.text = "0"
+    }
+
+    fun buttonAction(view: View) {
+        val button = view as Button
+
+        when (button.text) {
+            "+", "–", "x", "/" -> makeAction(button)
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" -> {numberButtonAction(button)}
+            "Clear" -> {clearAction()}
+            "." -> {dotAction()}
+            "=" -> {resultAction()}
+            else -> { print("Android 💩") }
+        }
+    }
+
+    fun clearAction() {
+        textView.text = "0"
+        this.lastResult = ""
+        this.slectedButton?.let { setDiselected(it) }
+        this.slectedButton = null
+    }
+
+    fun resultAction() {
+
+        if (this.slectedButton == null) {
+            return
+        }
+
+        if (this.isActionJustSelected) {
+            this.isActionJustSelected = false
+            this.slectedButton?.let { this.setDiselected(it) }
+            this.slectedButton = null
+            this.lastResult = ""
+            return
+        }
+        result()
+        this.slectedButton?.let { this.setDiselected(it) }
+        this.slectedButton = null
+        this.lastResult = ""
+        this.isActionJustSelected = true
+    }
+
+    fun result() {
+        val firstParam: Double? = lastResult.toFloatOrNull()?.toDouble()
+        val secondParam: Double? = textView.text.toString().toFloatOrNull()?.toDouble()
+        var result = 0.0
+        if ((firstParam != null) && (secondParam != null)) {
+            when (this.slectedButton?.text) {
+                "+" -> {result = firstParam + secondParam}
+                "–" -> {result = firstParam - secondParam}
+                "x" -> {result = firstParam * secondParam}
+                "/" -> {result = firstParam / secondParam}
+            }
+            if (!result.isNaN()) {
+                textView.text = result.toString()
+            } else {
+                textView.text = "Error"
+            }
+        }
+    }
+
+    fun makeAction(button: Button) {
+        if ((this.lastResult != "") && (!isActionJustSelected)) {
+            result()
+        }
+        this.isActionJustSelected = true
+        this.slectedButton?.let { setDiselected(it) }
+        this.slectedButton = button
+        if (textView.text != "0") {
+            this.lastResult = textView.text as String
+        }
+        setSelected(button)
+    }
+
+    fun dotAction() {
+        if (!textView.text.contains(".")) {
+            textView.text = textView.text.toString() + "."
+        }
+    }
+
+    fun numberButtonAction(button: Button) {
+        if (this.isActionJustSelected) {
+            textView.text = ""
+        }
+        this.isActionJustSelected = false
+        if (textView.text.length < 9) {
+            if (textView.text == "0") {
+                textView.text = button.text
+            } else {
+                textView.text = textView.text.toString() + button.text
+            }
+        }
+    }
+
+    fun setSelected(button: Button) {
+        button.setTextColor(Color.WHITE)
+    }
+
+    fun setDiselected(button: Button) {
+        button.setTextColor(Color.BLACK)
+    }
+}
